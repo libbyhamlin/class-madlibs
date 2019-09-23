617387885dfce028a07ac4bad2d1ec664e77e7b5
Libby Hamlin <bravolibby@gmail.com>
2019-09-22 19:40:48 -0700

    madlibs finally works.

--- app.js
+++ app.js
@@ -1,53 +1,43 @@
 function makeMadLib() {
     const nameOneInput = document.getElementById('name-one');
-    const nameOne = nameOneInput.value;
-    const nameOneSpan = document.getElementById('nameOne');
-    nameOneSpan.textContent = nameOne;
+    const nameOneSpan = document.getElementById('name-one-span');
+    nameOneSpan.textContent = nameOneInput.value;
 
     const friendNameInput = document.getElementById('friend-name');
-    const friendName = friendNameInput.value;
-    const friendNameSpan = document.getElementById('friendName');
-    friendNameSpan.textContent = friendName;
+    const friendNameSpan = document.getElementById('friend-name-span');
+    friendNameSpan.textContent = friendNameInput.value;
 
-    const aAnimalInput = document.getElementById('a-animal');
-    const aAnimal = aAnimalInput.value;
-    const aAnimalSpan = document.getElementById('aAnimal');
-    aAnimalSpan.textContent = aAnimal;
+    const animalInput = document.getElementById('animal');
+    const animalSpan = document.getElementById('animal-span');
+    animalSpan.textContent = animalInput.value;
 
-    const AdjectiveOneInput = document.getElementById('adjective-one');
-    const adjectiveOne = AdjectiveOneInput.value;
-    const adjectiveOneSpan = document.getElementById('adjectiveOne');
-    adjectiveOneSpan.textContent = adjectiveOne;
+    const adjectiveOneInput = document.getElementById('adjective-one');
+    const adjectiveSpan = document.getElementById('adjective-span');
+    adjectiveSpan.textContent = adjectiveOneInput.value;
 
     const monsterAnimalInput = document.getElementById('monster-animal');
-    const monsterAnimal = monsterAnimalInput.value;
-    const monsterAnimalSpan = document.getElementById('monsterAnimal');
-    monsterAnimalSpan.textContent = monsterAnimal;
+    const monsterAnimalSpan = document.getElementById('monster-animal-span');
+    monsterAnimalSpan.textContent = monsterAnimalInput.value;
 
     const verbOneInput = document.getElementById('verb-one');
-    const verbOne = verbOneInput.value;
-    const verbOneSpan = document.getElementById('verbOne');
-    verbOneSpan.textContent = verbOne;
+    const verbOneSpan = document.getElementById('verb-one-span');
+    verbOneSpan.textContent = verbOneInput.value;
 
     const nounOneInput = document.getElementById('noun-one');
-    const nounOne = nounOneInput.value;
-    const nounOneSpan = document.getElementById('nounOne');
-    nounOneSpan.textContent = nounOne;
+    const nounOneSpan = document.getElementById('noun-one-span');
+    nounOneSpan.textContent = nounOneInput.value;
 
     const nounTwoInput = document.getElementById('noun-two');
-    const nounTwo = nounTwoInput.value;
-    const nounTwoSpan = document.getElementById('nounTwo');
-    nounTwoSpan.textContent = nounTwo;
+    const nounTwoSpan = document.getElementById('noun-two-span');
+    nounTwoSpan.textContent = nounTwoInput.value;
 
     const pastVerbInput = document.getElementById('past-verb');
-    const pastVerb = pastVerbInput.value;
-    const pastVerbSpan = document.getElementById('pastVerb');
-    pastVerbSpan.textContent = pastVerb;
+    const pastVerbSpan = document.getElementById('past-verb-span');
+    pastVerbSpan.textContent = pastVerbInput.value;
 
     const aHomeInput = document.getElementById('a-home');
-    const aHome = aHomeInput.value;
-    const aHomeInputSpan = document.getElementById('aHome');
-    aHomeInputSpan.textContent = aHome;
+    const aHomeInputSpan = document.getElementById('home-span');
+    aHomeInputSpan.textContent = aHomeInput.value;
 
     const showMadLibStory = document.getElementById('madlib-story');
     showMadLibStory.classList.remove('hidden');
